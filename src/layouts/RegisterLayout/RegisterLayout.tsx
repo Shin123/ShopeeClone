3dee6bdca8ccd55bd15c29fa10f4a67e6a72ec6d
Shin123 <vanvietkhtn@gmail.com>
2023-11-16 22:47:55 +0700

    config react router

---
+++ src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {
+  children?: React.ReactNode
+}
+
+const RegisterLayout = ({ children }: Props) => {
+  return <div>RegisterLayout{children}</div>
+}
+
+export default RegisterLayout
