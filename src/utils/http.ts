ccab627a3d18a27a7a604321b15431c190ea4bbc
Shin123 <vanvietkhtn@gmail.com>
2023-11-22 00:00:56 +0700

    add api register

---
+++ src/utils/http.ts
@@ -0,0 +1,32 @@
+import axios, { HttpStatusCode, type AxiosInstance, AxiosError } from 'axios'
+import { toast } from 'react-toastify'
+
+class Http {
+  instance: AxiosInstance
+  constructor() {
+    this.instance = axios.create({
+      baseURL: import.meta.env.VITE_BASE_URL,
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response
+      },
+      function (error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
+  }
+}
+
+const http = new Http().instance
+
+export default http
