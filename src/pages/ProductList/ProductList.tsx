89e1f07a672076c345e3eae5b8f209e471dc2c86
VietNguyen <nvv7694@gmail.com>
2024-04-11 15:38:47 +0700

    fetch data and render product in productList

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -2,8 +2,20 @@ import React from 'react'
 import AsideFilter from './AsideFilter'
 import SortProductList from './SortProductList'
 import Product from './Product'
+import { useQuery } from '@tanstack/react-query'
+import useQueryParams from 'src/hooks/useQueryParams'
+import productApi from 'src/apis/product.api'
 
 const ProductList = () => {
+  const queryParams = useQueryParams()
+  const { data } = useQuery({
+    queryKey: ['products', queryParams],
+    queryFn: () => {
+      return productApi.getProduct(queryParams)
+    }
+  })
+
+  console.log(data)
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
@@ -14,13 +26,11 @@ const ProductList = () => {
           <div className='col-span-9'>
             <SortProductList />
             <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-              {Array(30)
-                .fill(0)
-                .map((_, index) => (
-                  <div className='col-span-1' key={index}>
-                    <Product />
-                  </div>
-                ))}
+              {data?.data.data.products.map((product) => (
+                <div className='col-span-1' key={product._id}>
+                  <Product product={product} />
+                </div>
+              ))}
             </div>
           </div>
         </div>
