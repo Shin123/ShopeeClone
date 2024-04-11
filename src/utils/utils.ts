c478238dd39296b2ed118d7c1b744021dfd87fc5
VietNguyen <nvv7694@gmail.com>
2024-04-11 15:20:43 +0700

    utils: format number

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,17 @@ import { AxiosError, HttpStatusCode, isAxiosError } from 'axios'
 export function isAxiosUnprocessableEntityError<FormError>(error: unknown): error is AxiosError<FormError> {
   return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
 }
+
+export function formatCurrency(currency: number) {
+  return new Intl.NumberFormat('de-DE').format(currency)
+}
+
+export function formatNumberToSocialStyle(value: number) {
+  return new Intl.NumberFormat('en', {
+    notation: 'compact',
+    maximumFractionDigits: 1
+  })
+    .format(value)
+    .replace('.', ',')
+    .toLowerCase()
+}
