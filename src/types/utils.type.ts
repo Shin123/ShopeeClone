261648a98006f884a00e5b6719785b8d9010acc9
Shin <vanvietkhtn@gmail.com>
2024-05-01 12:52:12 +0700

    add remove undefined type

--- src/types/utils.type.ts
+++ src/types/utils.type.ts
@@ -7,3 +7,9 @@ export interface ErrorResponse<Data> {
   message: string
   data?: Data
 }
+
+// remove undefined of key optional
+
+export type NoUndefinedField<T> = {
+  [P in keyof T]-?: NoUndefinedField<NonNullable<T[P]>>
+}
