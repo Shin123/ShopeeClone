72578c6fa931d739d4f5c01618253f098a64c030
Shin <vanvietkhtn@gmail.com>
2024-05-21 00:30:34 +0700

    add hook searchProducts

---
+++ src/hooks/useSearchProducts.tsx
@@ -0,0 +1,31 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { omit } from 'lodash'
+import { useForm } from 'react-hook-form'
+import { createSearchParams, useNavigate } from 'react-router-dom'
+import path from 'src/constants/path'
+import { Schema, schema } from 'src/utils/rules'
+import useQueryConfig from './useQueryConfig'
+
+type FormData = Pick<Schema, 'name'>
+const nameSchema = schema.pick(['name'])
+
+export default function useSearchProducts() {
+  const queryConfig = useQueryConfig()
+  const navigate = useNavigate()
+
+  const { register, handleSubmit } = useForm<FormData>({
+    defaultValues: { name: '' },
+    resolver: yupResolver(nameSchema)
+  })
+
+  const onSubmitSeach = handleSubmit((data) => {
+    const config = queryConfig.order
+      ? omit({ ...queryConfig, name: data.name }, ['order', 'sort_by'])
+      : { ...queryConfig, name: data.name }
+    navigate({
+      pathname: path.home,
+      search: createSearchParams(config).toString()
+    })
+  })
+  return { onSubmitSeach, register }
+}
